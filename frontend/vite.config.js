e15db7746f3b6cbc70257fbc871743dd9490df5b
박지호 <yoho4613@gmail.com>
2023-08-29 16:49:08 +1200

    emotion cache

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build:{
     rollupOptions: {
-      external: ["@emotion/react", "@emotion/*"],
+      external: ["@emotion/react", "@emotion/cache"],
       
     },
     outDir: "build"
